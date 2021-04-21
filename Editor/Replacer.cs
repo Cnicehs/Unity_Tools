3f1e0dd8f1d6f7585dbf9d9d5aa0806bd077d147
Cnicehs <39639405+Cnicehs@users.noreply.github.com>
2021-04-21 22:53:46 +0800

    Adapt UI

--- Editor/Replacer.cs
+++ Editor/Replacer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEditor;
 using System.Text.RegularExpressions;
 using UnityEditorInternal;
+using System.Linq;
 
 public class Replacer : EditorWindow
 {
@@ -21,9 +22,9 @@ public class Replacer : EditorWindow
     bool asChild;
     bool showChecked = true;
     public string pattren="";
+    public Vector2 scroll;
     void OnGUI()
     {
-        var properties = typeof(UISprite).GetProperties();
         temple = EditorGUILayout.ObjectField("Temple", temple, typeof(Component),true) as Component;
         root = EditorGUILayout.ObjectField("Root", root, typeof(Transform), true) as Transform;
         if(temple==null||root==null) 
@@ -39,63 +40,40 @@ public class Replacer : EditorWindow
             pattren = temple.name;
 
         // ifUsePosition = EditorGUILayout.Toggle("是否使用temple的Transform", ifUsePosition);
-        
-        if(GUILayout.Button("Start"))
+        List<Component> components = new List<Component>();
+
+        //GetAllComponents
+        foreach (var item in root.GetComponentsInChildren(asChild ? typeof(Transform) : temple.GetType(), true))
         {
-            if(asChild)
-            {
-                foreach (var item in root.GetComponentsInChildren(typeof(Transform), true))
-                {
-                    if (!Regex.IsMatch(item.name, pattren))
-                        continue;
-                    LogUtil.LogError(item.name);
-
-                    GameObject.Instantiate(temple,item.transform);
-
-                    // if (ComponentUtility.CopyComponent(temple))
-                    // {
-                    //     ComponentUtility.PasteComponentValues(item);
-                    // }
-                }
-            }else
-            {
-                foreach (var item in root.GetComponentsInChildren(temple.GetType(), true))
-                {
-                    if (!Regex.IsMatch(item.name, pattren))
-                        continue;
-                    LogUtil.LogError(item.name);
-
-                    // item.spriteName = temple.spriteName;
-                    // item.atlas = temple.atlas;
-                    // item.keepAspectRatio = temple.keepAspectRatio;
-                    // item.aspectRatio = temple.aspectRatio;
-                    // item.width = temple.width;
-                    // item.height = temple.height;
-                    // item.pivot = temple.pivot;
-
-
-                    // if(item.GetType() == temple.GetType())
-                    // {
-                    if (ComponentUtility.CopyComponent(temple))
-                    {
-                        ComponentUtility.PasteComponentValues(item);
-                    }
-                    // }
-
-
-                    // if(ifUsePosition)
-                    // {
-                    //     item.transform.localScale = temple.transform.localScale;
-                    //     item.transform.localEulerAngles = temple.transform.localEulerAngles;
-                    //     item.transform.localPosition = temple.transform.localPosition;
-                    // }
-                }
-            }
+            if (!Regex.IsMatch(item.name, pattren))
+                continue;
+            components.Add(item);
         }
 
-        showChecked = EditorGUILayout.Toggle("预览选中物体", showChecked);
+
+        if(GUILayout.Button("Start"))
+        {
+                foreach (var item in components)
+                {
+                    Debug.LogError(item.name);
+                    if(asChild)
+                    {
+                        GameObject.Instantiate(temple, item.transform);
+                    }else
+                    {
+                        if (ComponentUtility.CopyComponent(temple))
+                        {
+                            ComponentUtility.PasteComponentValues(item);
+                        }
+                    }
+                }
+        }
+
+        // showChecked = EditorGUILayout.Toggle("预览选中物体", showChecked);
+        showChecked = EditorGUILayout.Foldout(showChecked, "选中的对象");
         if(showChecked)
         {
+            scroll = EditorGUILayout.BeginScrollView(scroll);
             Object tmp;
             foreach (var item in root.GetComponentsInChildren(asChild?typeof(Transform):temple.GetType(), true))
             {
@@ -103,8 +81,20 @@ public class Replacer : EditorWindow
                     continue;
                 // if (item.GetType() != temple.GetType())
                 //     continue;
-                tmp = EditorGUILayout.ObjectField("Temple", item, typeof(UISprite), true) as UISprite;
+                tmp = EditorGUILayout.ObjectField("Temple", item, item.GetType(),true);
             }
+            EditorGUILayout.EndScrollView();
         }
+    }
+
+    bool AncestorIs(Transform obj,params string[] names)
+    {
+        while(obj.parent!=null)
+        {
+            if(names.Contains(obj.parent.name))
+                return true;
+            obj = obj.parent;
+        }
+        return false;
     }
 }
